e6b0d2816e1c4a8d04b7cde64cbf6af4a6dc175c
StoutPanda <jholder29@gmail.com>
2016-10-09 13:51:32 -0500

    adding validation

--- length_convert.ex
+++ length_convert.ex
@@ -1,15 +1,8 @@
 # Meter To Feet Conversion
 
 defmodule MeterToLengthConverter  do
-  defmodule Feet do
-    def convert(m) do
-      m * 3.28084
-    end
-  end
-
-  defmodule Inch do
-    def convert(m) do
-      m * 39.3701
-    end
-  end
+  def convert(:feet, m) when is_number(m) and m >= 0, do: m * 3.28084
+  def convert(:inch, m) when is_number(m) and m >= 0, do: m * 39.3701
+  def convert(:yard, m) when is_number(m) and m >= 0, do: m * 1.09361
 end
+div
