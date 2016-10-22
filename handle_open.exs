b781812191308145ed09c676ae2c04a1f99156c8
Stout Panda <jholder29@gmail.com>
2016-10-22 09:13:31 -0500

    A few files working through programming elixir.

---
+++ handle_open.exs
@@ -0,0 +1,9 @@
+#handle_open.exs
+handle_open = fn
+            {:ok, file} -> "First line: #{IO.read(file,:line)}"
+            {_,  error} -> "Error: #{:file.format_error(error)}"
+end
+
+IO.puts handle_open.(File.open("test.txt"))
+IO.puts handle_open.(File.open("fakefileblah"))
+
