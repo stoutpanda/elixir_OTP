85f6651c7d32f0effada8cda3b40183bbb35f966
Stout Panda <jholder29@gmail.com>
2016-11-03 21:12:34 -0500

    Added data transformations and test through module table_formater

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -4,6 +4,7 @@ defmodule Issues.CLI do
   Handle the vommand line parsing and the dispatch to the various functions
   that end up generating a table of the last _n_ issues in a github project.
   """
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
 
   def run(argv) do
     argv
@@ -48,6 +49,7 @@ defmodule Issues.CLI do
     |> decode_response
     |> sort_into_ascending_order
     |> Enum.take(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({:ok, body}), do: body
