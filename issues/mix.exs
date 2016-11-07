46c8a1e89767e289291e2b5b71b54e29061d72cd
Stout Panda <jholder29@gmail.com>
2016-11-07 16:58:47 -0600

    added and generated basic documentation

--- issues/mix.exs
+++ issues/mix.exs
@@ -3,6 +3,7 @@ defmodule Issues.Mixfile do
 
   def project do
     [app: :issues,
+     name: "Issues",
      version: "0.1.0",
      escript: escript_config,
      elixir: "~> 1.3",
@@ -17,12 +18,14 @@ defmodule Issues.Mixfile do
 
   defp deps do
     [
-      httpoison: "~> 0.9",
-      poison: "~> 2.2"
+      { :httpoison, "~> 0.9" },
+      { :poison, "~> 2.2" },
+      { :ex_doc, "~> 0.12" },
+      { :earmark, "~> 1.0", override: true }
     ]
   end
 
   defp escript_config do
-    [main_module: Issues.CLI] 
+    [ main_module: Issues.CLI ]
   end
 end
