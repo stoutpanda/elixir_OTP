6b7ce104f44dfbc525324f02046e28c368a78fb3
Stout Panda <jholder29@gmail.com>
2016-11-07 16:26:32 -0600

    created main function, updated mix to use escript for compiling and included new compiled file.

--- issues/mix.exs
+++ issues/mix.exs
@@ -4,32 +4,25 @@ defmodule Issues.Mixfile do
   def project do
     [app: :issues,
      version: "0.1.0",
+     escript: escript_config,
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       httpoison: "~> 0.9",
       poison: "~> 2.2"
     ]
+  end
+
+  defp escript_config do
+    [main_module: Issues.CLI] 
   end
 end
